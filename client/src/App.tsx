f1b24cc8c93506226e8d5bcb65d81773d8b6963f
Emmybest1 <emmajerry2016@gmail.com>
2022-02-14 08:09:32 +0200

    Initial: initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p>Welcome home</p>;
+}
+
+export default App;
---
+++ server/src/utils/const.js
@@ -0,0 +1,5 @@
+const possibleMethods = ['HEAD', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH', '*'];
+
+module.exports = {
+  possibleMethods,
+};
