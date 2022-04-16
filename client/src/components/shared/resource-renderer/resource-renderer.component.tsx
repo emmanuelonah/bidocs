6ed140262e2ff8e3b7d3c593bbd927f2bb2472d3
emmanuelonah <emmajerry2016@gmail.com>
2022-04-16 11:02:44 +0200

    Feature: added a patternic resource renderer for uniformism

---
+++ client/src/components/shared/resource-renderer/resource-renderer.component.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { __dev__ } from 'utils';
+import { Loader, Alerter } from 'components/shared';
+
+const DISPLAY_NAME = 'ResourceRenderer';
+
+type PrimitiveDivProps = React.ComponentPropsWithoutRef<'div'>;
+type ResourceRendererElement = React.ElementRef<'div'>;
+interface ResourceRendererProps extends PrimitiveDivProps {
+  isLoading: boolean;
+  error: string | null;
+  data: any;
+  // eslint-disable-next-line no-unused-vars
+  children: <DataType>(data: DataType) => React.ReactElement;
+}
+
+const ResourceRenderer = React.forwardRef<ResourceRendererElement, ResourceRendererProps>(
+  (props, forwardedRef) => {
+    const { isLoading, error, data, children, className, ...restProps } = props;
+
+    if (isLoading) {
+      return <Loader isLoading={isLoading} />;
+    }
+
+    if (error) {
+      return (
+        <Alerter aria-atomic="true" aria-live="assertive" type="danger">
+          {error}
+        </Alerter>
+      );
+    }
+
+    return (
+      <div
+        {...restProps}
+        className={`resource-renderer-container ${className ?? ''}`}
+        ref={forwardedRef}
+      >
+        {children(data)}
+      </div>
+    );
+  }
+);
+
+if (__dev__) {
+  ResourceRenderer.displayName = DISPLAY_NAME;
+}
+
+export { ResourceRenderer };
