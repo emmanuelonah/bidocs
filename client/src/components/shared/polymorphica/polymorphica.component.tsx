6d38d35fe7dcffcdcef10900d7454e7e27683a5d
emmanuelonah <emmajerry2016@gmail.com>
2022-04-17 21:24:21 +0200

    Feature: initial draft of a generic polymorphic component

--- client/src/components/shared/polymorphica/polymorphica.component.tsx
+++ client/src/components/shared/polymorphica/polymorphica.component.tsx
@@ -1,1 +1,31 @@
-export {};
+import React from 'react';
+
+import { __dev__ } from 'utils';
+
+const DISPLAY_NAME = 'Polymorphica';
+
+type PrimitiveProps<T extends React.ElementType> = {
+  as: T;
+  forwardedRef: React.MutableRefObject<T>;
+};
+
+type PolymorphicaProps<T extends React.ElementType> = {} & Omit<
+  React.ComponentPropsWithoutRef<T>,
+  keyof PrimitiveProps<T>
+>;
+
+function Polymorphica<T extends React.ElementType>({
+  as,
+  forwardedRef,
+  ...restProps
+}: PolymorphicaProps<T>) {
+  const Component = as;
+
+  return <Component {...restProps} ref={forwardedRef} />;
+}
+
+if (__dev__) {
+  Polymorphica.displayName = DISPLAY_NAME;
+}
+
+export { Polymorphica };
--- client/src/components/shared/polymorphica/polymorphica.stories.tsx
+++ client/src/components/shared/polymorphica/polymorphica.stories.tsx
@@ -1,1 +1,24 @@
-export {};
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { __storybook__ } from 'utils';
+
+import { Polymorphica } from './polymorphica.component';
+
+export default {
+  title: `${__storybook__.TITLES.shared}/Polymorphica`,
+  component: Polymorphica,
+  argTypes: {
+    disabled: { control: { type: 'boolean' } },
+  },
+} as ComponentMeta<typeof Polymorphica>;
+
+export const Link: ComponentStory<typeof Polymorphica> = () => (
+  <Polymorphica as="a" href="https://emmanuelonah.com" target="_blank">
+    Hello i am a ploymorphic link
+  </Polymorphica>
+);
+
+export const Button: ComponentStory<typeof Polymorphica> = () => (
+  <Polymorphica as="button">Hello i am a ploymorphic button</Polymorphica>
+);
