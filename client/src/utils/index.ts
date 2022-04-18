9dd3dc217193e1ae20ea92ed12ff78ffbead5d16
emmanuelonah <emmajerry2016@gmail.com>
2022-04-18 11:30:17 +0200

    Feaure: added storybook util

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './env';
 export * from './error';
 export * from './theme';
+export * from './storybook';
 export * from './array-yielder';
 export * from './compose-event';
 export * from './create-context';
---
+++ client/src/utils/storybook.ts
@@ -0,0 +1,9 @@
+const TITLES = Object.freeze({
+  page: 'Page',
+  layout: 'Layout',
+  shared: 'Shared',
+});
+
+export const __storybook__ = {
+  TITLES,
+};
